5a65bc2e1a64bd08f15a4267cbb260c59a092bbb
Mauricio de Assis <mauriciox8mm@hotmail.com>
2022-10-16 11:05:56 -0300

    projeto inicial

--- ProjetoListaAlunos/src/main/java/com/mycompany/projetolistaalunos/ProjetoListaAlunos.java
+++ ProjetoListaAlunos/src/main/java/com/mycompany/projetolistaalunos/ProjetoListaAlunos.java
@@ -2,8 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.projetolistaalunos;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,9 +16,37 @@ package com.mycompany.projetolistaalunos;
 public class ProjetoListaAlunos {
 
     public static void main(String[] args) {
-        
-        
-       /// asdasd
-        System.out.println("Hello World!");
+
+        int contador;
+        List<Aluno> listaAluno = new ArrayList<>();
+        Aluno a1 = new Aluno();
+        Vetor lista = new Vetor();
+
+        try {
+
+            do {
+                contador = Integer.parseInt(JOptionPane.showInputDialog("[1] Adicionar um aluno\n"
+                        + "[2] Ver os alunos \n"
+                        + "[5] Sair"));
+                /**
+                 * Adicionar Aluno
+                 */
+                if (contador == 1) {
+                    a1 = new Aluno();
+                    a1.setNome(JOptionPane.showInputDialog("Digite um nome \n"));
+                    listaAluno = lista.adiciona(a1);
+                    /**
+                     * Listar alunos
+                     */
+                } else if (contador == 2) {
+                    JOptionPane.showMessageDialog(null, listaAluno.toArray());
+                } else if (contador == 5) {
+                    JOptionPane.showMessageDialog(null, "Saindo");
+                }
+            } while (contador != 5);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro" + e.getMessage());
+        }
+
     }
 }
---
+++ ProjetoListaAlunos/src/main/java/com/mycompany/projetolistaalunos/Vetor.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetolistaalunos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author User
+ */
+public class Vetor {
+
+    // Declarando e Inicializando um array de Aluno com capacidade 100.
+    private List<Aluno> listaAlunos = new ArrayList();
+
+    public List<Aluno> adiciona(Aluno aluno) {
+        listaAlunos.add(aluno);
+        return listaAlunos;
+    }
+
+    public void adiciona(int posicao, Aluno aluno) {
+// implementação
+    }
+
+    public Aluno pega(int posicao) {
+// implementação
+        return null;
+// implementação
+    }
+
+    public void remove(int posicao) {
+// implementação
+    }
+
+    public boolean contem(Aluno aluno) {
+// implementação
+        return false;
+// implementação
+    }
+
+    public int tamanho() {
+// implementação
+        return 0;
+// implementação
+    }
+
+}
